ffa671d29b7479df3f700f3cd694b52b8197c3d7
Stanislav Popov <popstas@gmail.com>
2020-05-30 00:49:41 +0500

    feat: add gy-49 lux, remove pir sensor and ssd1306 display

--- fs/init.js
+++ fs/init.js
@@ -1,7 +1,9 @@
+// mongoose-button-bme
 load('api_arduino_bme280.js');
 load('api_arduino_ssd1306.js');
 load('api_config.js');
 load('api_esp8266.js');
+load('api_adc.js');
 load('api_gpio.js');
 load('api_i2c.js');
 load('api_http.js');
@@ -68,13 +70,15 @@ if (requestUrl) {
   );
 } else {
   print('No requestUrl defined! you should define it with mos config-set webhook=https://your-url');
-  blink(250);
+  // blink(250);
 }
 
 let last = {
   temp: "0",
   hum: "0",
-  press: "0"
+  press: "0",
+  brightness: "0",
+  lux: "0"
 };
 
 // BME280 sensor
@@ -87,88 +91,102 @@ if (bme === undefined) {
   print('Cant find a sensor');
 } else {
   print('bme280 connected!');
-
-  Timer.set(
-    1000,
-    true,
-    function() {
-      let data = {
-        temperature: bme.readTemperature(),
-        humidity: bme.readHumidity(),
-        pressure: bme.readPressure()
-      };
-
-      let current = {
-        temp: JSON.stringify(Math.round(data.temperature)),
-        hum: JSON.stringify(Math.round(data.humidity)),
-        press: JSON.stringify(Math.round(data.pressure))
-      };
-
-      // serial output
-      print('bme280 temperature: ', data.temperature);
-      print('bme280 humidity: ', data.humidity);
-      print('bme280 pressure: ', data.pressure);
-
-      // oled display
-      /* showStr(
-        d,
-        'tmp: ' +
-        current.temperature +
-          '\nhum: ' +
-          current.humidity +
-          '\nprs: ' +
-          current.pressure
-      ); */
-
-      let base = Cfg.get('app.mqtt_base');
-
-      // publish to mqtt metrics if changed
-      print('before');
-      let names = ['temp', 'hum', 'press'];
-      for(let i in names){
-        let m = names[i];
-        if(current[m] !== last[m]){
-          print('mqtt: ' + m + ': ' + current[m] + ', last: ' + last[m]);
-          MQTT.pub(base + '/' + m, current[m]);
-        }
-        last[m] = current[m];
-      }
-    },
-    null
-  );
 }
 
-// PIR sensor
-GPIO.set_mode(Cfg.get('app.pir_pin'), GPIO.MODE_INPUT); // D8
-/* Timer.set(
+// MAX44009 (GY-49) light sensor
+print('Init GY-49');
+let gy49_addr = 74; //0x4A;
+let gy49 = I2C.get();
+// 0x02 - Configuration register
+// 0x00 - Automatic mode, Integration time = 800 ms, for low-light
+// 0x40 - Manual mode
+I2C.writeRegB(gy49, gy49_addr, 0x02, 0x00);
+
+function gy49_read() {
+  // let data = I2C.readRegN(gy49, gy49_addr, 0x03);
+  let data1 = I2C.readRegB(gy49, gy49_addr, 0x03);
+  let data2 = I2C.readRegB(gy49, gy49_addr, 0x04);
+  // print('data1: ', data1);
+  // print('data2: ', data2);
+    
+  // Convert the data to lux
+  let exponent = (data1 & 0xF0) >> 4;
+  let mantissa = ((data1 & 0x0F) << 4) | (data2 & 0x0F);
+  let luminance = Math.pow(2, exponent) * mantissa * 0.045;
+  // print('exponent: ', exponent);
+  // print('mantissa: ', mantissa);
+  return luminance;
+}
+
+Timer.set(
   1000,
   true,
   function() {
-    let pir = GPIO.read(Cfg.get('app.pir_pin'));
+    let data = {
+      temperature: bme.readTemperature(),
+      humidity: bme.readHumidity(),
+      pressure: bme.readPressure(),
+      brightness: ADC.read(0) / 1024 * 100,
+      lux: gy49_read()
+    };
 
-    GPIO.write(Cfg.get('app.led_pin'), Math.abs(1 - pir));
+    
+    let current = {
+      temp: JSON.stringify(Math.round(data.temperature)),
+      hum: JSON.stringify(Math.round(data.humidity)),
+      press: JSON.stringify(Math.round(data.pressure)),
+      brightness: JSON.stringify(Math.round(data.brightness)),
+      lux: JSON.stringify(Math.round(data.lux))
+    };
+
+    // serial output
+    print('brightness: ', data.brightness);
+    print('bme280 temperature: ', data.temperature);
+    print('bme280 humidity: ', data.humidity);
+    print('bme280 pressure: ', data.pressure);
+    print('gy-49 lux: ', data.lux);
+
+    // oled display
+    /* showStr(
+      d,
+      'tmp: ' +
+      current.temperature +
+        '\nhum: ' +
+        current.humidity +
+        '\nprs: ' +
+        current.pressure
+    ); */
 
     let base = Cfg.get('app.mqtt_base');
-    print(base + '/pir ' + JSON.stringify(pir));
-    MQTT.pub(base + '/pir', JSON.stringify(pir));
+
+    // publish to mqtt metrics if changed
+    let names = ['temp', 'hum', 'press', 'brightness', 'lux'];
+    for(let i in names){
+      let m = names[i];
+      if(current[m] !== last[m]){
+        print('mqtt: ' + m + ': ' + current[m] + ', last: ' + last[m]);
+        MQTT.pub(base + '/' + m, current[m]);
+      }
+      last[m] = current[m];
+    }
   },
   null
-); */
+);
 
 // display
-print('initialize display...');
-let d = Adafruit_SSD1306.create_i2c(4, Adafruit_SSD1306.RES_128_64);
-d.begin(Adafruit_SSD1306.SWITCHCAPVCC, 0x3c, true /* reset */);
-d.display();
+// print('initialize display...');
+// let d = Adafruit_SSD1306.create_i2c(4, Adafruit_SSD1306.RES_128_64);
+// d.begin(Adafruit_SSD1306.SWITCHCAPVCC, 0x3c, true /* reset */);
+// d.display();
 
-let showStr = function(d, str) {
-  d.clearDisplay();
-  d.setTextSize(2);
-  d.setTextColor(Adafruit_SSD1306.WHITE);
-  d.setCursor(0, 0);
-  d.write(str);
-  d.display();
-};
+// let showStr = function(d, str) {
+//   d.clearDisplay();
+//   d.setTextSize(2);
+//   d.setTextColor(Adafruit_SSD1306.WHITE);
+//   d.setCursor(0, 0);
+//   d.write(str);
+//   d.display();
+// };
 
 // wifi connect event
 /* Event.addGroupHandler(
@@ -179,15 +197,3 @@ let showStr = function(d, str) {
   },
   null
 ); */
-
-//trying mqtt
-/* MQTT.setEventHandler(function(conn, ev, edata) {
-  if (ev === MQTT.EV_CONNACK) {
-    let topic = '/feeds/' + feedName;
-    let ok = MQTT.pub(topic, JSON.stringify(1), 1);
-    if (ok) print('sent mqtt');
-    else print('failed mqtt');
-    MQTT.setEventHandler(function(){}, null);
-  }
-}, null);
- */
